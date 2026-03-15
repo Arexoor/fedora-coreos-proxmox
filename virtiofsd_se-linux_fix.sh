26efb65c2bdd43afbd2293c7d078d54d324099ab
Arexoor <silkashier@gmail.com>
2026-03-15 21:55:16 +0100

    created virtiofs script with dpkg-divert

---
+++ virtiofsd_se-linux_fix.sh
@@ -0,0 +1,9 @@
+VIRTIOFS_LINK="/usr/lib/qemu/virtiofsd"
+
+# Original nach virtiofsd.orig umleiten
+dpkg-divert --local --rename --add "$VIRTIOFS_LINK"
+
+cat > ${VIRTIOFS_LINK} << EOF
+#!/bin/bash
+exec "${VIRTIOFS_LINK}.orig" --xattrmap=:map::user.virtiofs.: --security-label "\$@"
+EOF
