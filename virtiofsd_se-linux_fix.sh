b51c7a3e2d5a5dfd78036d387387171119d70c2a
Arexoor <silkashier@gmail.com>
2026-07-10 21:04:23 +0200

    changed virtiofs config

--- virtiofsd_se-linux_fix.sh
+++ virtiofsd_se-linux_fix.sh
@@ -5,7 +5,10 @@ dpkg-divert --local --rename --add "$VIRTIOFS_LINK"
 
 cat > ${VIRTIOFS_LINK} << EOF
 #!/bin/bash
-exec "${VIRTIOFS_LINK}.distrib" --xattrmap=:map::user.virtiofs.: --security-label "\$@"
+# trusted.* statt user.*: user-xattrs sind auf symlinks nicht erlaubt (EPERM),
+# das SELinux-Label des Gasts wuerde daher jede symlink-Erstellung scheitern
+# lassen. trusted.* braucht CAP_SYS_ADMIN, daher --modcaps.
+exec "${VIRTIOFS_LINK}.distrib" --xattrmap=:map::trusted.virtiofs.: --security-label --modcaps=+sys_admin "\$@"
 EOF
 
 chmod +x ${VIRTIOFS_LINK}
